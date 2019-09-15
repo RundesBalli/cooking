34b0242c1c6515282cf8cc71241c91c412748810
RundesBalli <rundesballi@rundesballi.com>
2019-09-15 19:53:56 +0200

    Erstes Hinzufügen der Projektdateien.

---
+++ inc/start.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * start.php
+ * 
+ * Startseite mit kurzer Begrüßung und Erläuterung...
+ */
+$title = "Startseite";
+$content.= "<h1>pr0.cooking</h1>".PHP_EOL;
+$content.= "<div class='row'>".PHP_EOL.
+"<div class='col-x-12 col-s-12 col-m-12 col-l-12 col-xl-12'>Willkommen auf pr0.cooking!</div>".PHP_EOL.
+"<div class='col-x-12 col-s-12 col-m-12 col-l-12 col-xl-12'>pr0.cooking ist eine Rezeptsammlung für leckere Rezepte.</div>".PHP_EOL.
+"<div class='col-x-12 col-s-12 col-m-12 col-l-12 col-xl-12'>Rezeptvorschläge bitte an <a href='https://pr0gramm.com/inbox/messages/Nezos' target='_blank' rel='noopener'>Nezos</a>.</div>".PHP_EOL.
+"</div>".PHP_EOL;
+
+$content.= "<div class='spacer-l'></div>".PHP_EOL;
+
+/**
+ * ...sowie Auflistung aller Kategorien mit Kurzbeschreibung.
+ */
+$content.= "<h1>Kategorien</h1>".PHP_EOL;
+$content.= "<div class='row'>".PHP_EOL;
+$result = mysqli_query($dbl, "SELECT * FROM `categories` ORDER BY `sortindex` ASC, `title` ASC") OR DIE(MYSQLI_ERROR($dbl));
+while($row = mysqli_fetch_array($result)) {
+  $content.= "<div class='row'>".PHP_EOL;
+  $content.= "<div class='col-x-12 col-s-12 col-m-5 col-l-4 col-xl-3'><a href='/category/".$row['shortTitle']."'>".$row['title']."</a></div>".PHP_EOL.
+  "<div class='col-x-12 col-s-12 col-m-7 col-l-8 col-xl-9'>".($row['shortDescription'] == NULL ? "<span class='italic'>Keine Beschreibung vorhanden</span>" : $row['shortDescription'])."</div>".PHP_EOL.
+  "<div class='col-x-12 col-s-12 col-m-0 col-l-0 col-xl-0'><div class='spacer-s'></div></div>".PHP_EOL;
+  $content.= "</div>".PHP_EOL;
+}
+$content.= "</div>".PHP_EOL;
+?>
---
+++ src/template.tpl
@@ -0,0 +1,36 @@
+<!--
+Betrieben von:
+https://nezos.wtf
+
+Entwicklung:
+https://RundesBalli.com
+https://github.com/RundesBalli/pr0cooking
+-->
+<!DOCTYPE html>
+<html lang="de">
+  <head>
+    <title>pr0.cooking{TITLE}</title>
+    <meta charset="utf-8">
+    <meta name="description" content="pr0gramm Rezeptsammlung"/>
+    <meta name="robots" content="index, follow"/>
+    <meta name="keywords" content="pr0gramm, Rezept, Rezepte, Rezeptsammlung, Essen, lecker"/>
+    <meta name="author" content="Nezos"/>
+    <meta name="revisit-after" content="3 days"/>
+    <link href="/src/style.css" rel="stylesheet">
+    <script type="text/javascript" src="/src/toggleMenu.js"></script>
+    <link rel="shortcut icon" href="/src/favicon.png" type="image/png">
+    <meta name="theme-color" content="#161618">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  </head>
+  <body>
+    <div id="sidebar">
+      <img src="/src/header.png" alt="Header">
+      <a class="toggle" id="toggle"></a>
+      <a href="/">Startseite</a>
+      {NAV}
+    </div>
+    <div id="content">
+      {CONTENT}
+    </div>
+  </body>
+</html>
