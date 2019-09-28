c89bc42ce70fb8cb025d780a7de88ba4cd140a06
RundesBalli <rundesballi@rundesballi.com>
2019-09-28 17:15:16 +0200

    Vergessene EOLs hinzugefügt. Anzeige für leere Kategorien und nicht zugewiesene Rezepte hinzugefügt.

--- inc/adminindex.php
+++ inc/adminindex.php
@@ -44,9 +44,9 @@ $content.= "<div class='row hover'>".PHP_EOL.
 "<div class='col-x-12 col-s-12 col-m-0 col-l-0 col-xl-0'><div class='spacer-s'></div></div>".PHP_EOL.
 "</div>".PHP_EOL;
 
-$content.= "<div class='spacer-m'></div>";
+$content.= "<div class='spacer-m'></div>".PHP_EOL;
 
-$content.= "<h1>Am meisten geklickte Rezepte</h1>";
+$content.= "<h1>Am meisten geklickte Rezepte</h1>".PHP_EOL;
 $result = mysqli_query($dbl, "SELECT * FROM `most_clicked`") OR DIE(MYSQLI_ERROR($dbl));
 $top = 0;
 while($row = mysqli_fetch_array($result)) {
@@ -58,9 +58,9 @@ while($row = mysqli_fetch_array($result)) {
   "</div>".PHP_EOL;
 }
 
-$content.= "<div class='spacer-m'></div>";
+$content.= "<div class='spacer-m'></div>".PHP_EOL;
 
-$content.= "<h1>Am besten bewertete Rezepte</h1>";
+$content.= "<h1>Am besten bewertete Rezepte</h1>".PHP_EOL;
 $result = mysqli_query($dbl, "SELECT * FROM `best_voted`") OR DIE(MYSQLI_ERROR($dbl));
 $top = 0;
 while($row = mysqli_fetch_array($result)) {
@@ -73,5 +73,30 @@ while($row = mysqli_fetch_array($result)) {
   "</div>".PHP_EOL;
 }
 
+$result = mysqli_query($dbl, "SELECT `id`, `title` FROM `categories` WHERE NOT EXISTS (SELECT * FROM `category_items` WHERE `categories`.`id`=`category_items`.`category_id`)") OR DIE(MYSQLI_ERROR($dbl));
+if(mysqli_num_rows($result) != 0) {
+  $content.= "<div class='spacer-m'></div>".PHP_EOL;
+  $content.= "<h1 class='warn'>Leere Kategorien</h1>".PHP_EOL;
+  while($row = mysqli_fetch_array($result)) {
+    $content.= "<div class='row hover'>".PHP_EOL.
+    "<div class='col-x-12 col-s-12 col-m-9 col-l-9 col-xl-9'>".$row['title']."</div>".PHP_EOL.
+    "<div class='col-x-12 col-s-12 col-m-3 col-l-3 col-xl-3'><a href='/admincategories/edit/".$row['id']."' class='nowrap'>Bearbeiten</a> - <a href='/admincategories/assign/".$row['id']."' class='nowrap'>Rezepte zuweisen</a></div>".PHP_EOL.
+    "<div class='col-x-12 col-s-12 col-m-0 col-l-0 col-xl-0'><div class='spacer-s'></div></div>".PHP_EOL.
+    "</div>".PHP_EOL;
+  }
+}
+
+$result = mysqli_query($dbl, "SELECT `id`, `title` FROM `items` WHERE NOT EXISTS (SELECT * FROM `category_items` WHERE `items`.`id`=`category_items`.`item_id`)") OR DIE(MYSQLI_ERROR($dbl));
+if(mysqli_num_rows($result) != 0) {
+  $content.= "<div class='spacer-m'></div>".PHP_EOL;
+  $content.= "<h1 class='warn'>Nicht in Kategorien eingeteilte Rezepte</h1>".PHP_EOL;
+  while($row = mysqli_fetch_array($result)) {
+    $content.= "<div class='row hover'>".PHP_EOL.
+    "<div class='col-x-12 col-s-12 col-m-9 col-l-9 col-xl-9'>".$row['title']."</div>".PHP_EOL.
+    "<div class='col-x-12 col-s-12 col-m-3 col-l-3 col-xl-3'><a href='/adminitems/edit/".$row['id']."' class='nowrap'>Bearbeiten</a><br><a href='/adminitems/assign/".$row['id']."' class='nowrap'>Zuweisen</a></div>".PHP_EOL.
+    "<div class='col-x-12 col-s-12 col-m-0 col-l-0 col-xl-0'><div class='spacer-s'></div></div>".PHP_EOL.
+    "</div>".PHP_EOL;
+  }
+}
 
 ?>
