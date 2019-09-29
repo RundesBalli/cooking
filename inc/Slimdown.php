e02411e5eb1c7f5e229bd618ecf144518c647d2b
RundesBalli <rundesballi@rundesballi.com>
2019-09-29 21:31:20 +0200

    Paragraph Funktion wieder hinzugefügt.

--- inc/Slimdown.php
+++ inc/Slimdown.php
@@ -35,10 +35,20 @@ class Slimdown {
     '/`(.*?)`/' => '<code>\1</code>',                                                            // inline code
     '/\n\*(.*)/' => 'self::ul_list',                                                             // ul lists
     '/\n[0-9]+\.(.*)/' => 'self::ol_list',                                                       // ol lists
+    '/\n([^\n]+)\n/' => 'self::para',                                                            // add paragraphs
     '/<\/ul>\s?<ul>/' => '',                                                                     // fix extra ul
     '/<\/ol>\s?<ol>/' => ''                                                                      // fix extra ol
   );
 
+  private static function para ($regs) {
+    $line = $regs[1];
+    $trimmed = trim ($line);
+    if (preg_match ('/^<\/?(ul|ol|li|h|p|bl)/', $trimmed)) {
+      return "\n" . $line . "\n";
+    }
+    return sprintf ("\n<p>%s</p>\n", $trimmed);
+  }
+  
   private static function ul_list ($regs) {
     $item = $regs[1];
     return sprintf ("\n<ul>\n\t<li>%s</li>\n</ul>", trim ($item));
