8b46f2ecb4fb2f8814e82481a1f9e6655c612e6c
RundesBalli <rundesballi@rundesballi.com>
2019-09-29 18:56:31 +0200

    Überschriften und Rand hinzugefügt.

--- inc/adminindex.php
+++ inc/adminindex.php
@@ -18,27 +18,33 @@ $content.= "<div class='row'>".PHP_EOL.
 "</div>".PHP_EOL;
 
 $content.= "<h1>Zahlen, Daten, Fakten</h1>".PHP_EOL;
+$content.= "<div class='row highlight bold bordered'>".PHP_EOL.
+"<div class='col-x-12 col-s-12 col-m-3 col-l-3 col-xl-3'>Bezeichnung</div>".PHP_EOL.
+"<div class='col-x-12 col-s-12 col-m-9 col-l-9 col-xl-9'>Wert</div>".PHP_EOL.
+"<div class='col-x-12 col-s-12 col-m-0 col-l-0 col-xl-0'><div class='spacer-s'></div></div>".PHP_EOL.
+"</div>".PHP_EOL;
+
 $result = mysqli_query($dbl, "SELECT * FROM `stats`") OR DIE(MYSQLI_ERROR($dbl));
 $row = mysqli_fetch_array($result);
-$content.= "<div class='row hover'>".PHP_EOL.
+$content.= "<div class='row hover bordered'>".PHP_EOL.
 "<div class='col-x-12 col-s-12 col-m-3 col-l-3 col-xl-3'>Anzahl Kategorien</div>".PHP_EOL.
 "<div class='col-x-12 col-s-12 col-m-9 col-l-9 col-xl-9'>".$row['cat_count']."</div>".PHP_EOL.
 "<div class='col-x-12 col-s-12 col-m-0 col-l-0 col-xl-0'><div class='spacer-s'></div></div>".PHP_EOL.
 "</div>".PHP_EOL;
 
-$content.= "<div class='row hover'>".PHP_EOL.
+$content.= "<div class='row hover bordered'>".PHP_EOL.
 "<div class='col-x-12 col-s-12 col-m-3 col-l-3 col-xl-3'>Anzahl Rezepte</div>".PHP_EOL.
 "<div class='col-x-12 col-s-12 col-m-9 col-l-9 col-xl-9'>".$row['item_count']."</div>".PHP_EOL.
 "<div class='col-x-12 col-s-12 col-m-0 col-l-0 col-xl-0'><div class='spacer-s'></div></div>".PHP_EOL.
 "</div>".PHP_EOL;
 
-$content.= "<div class='row hover'>".PHP_EOL.
+$content.= "<div class='row hover bordered'>".PHP_EOL.
 "<div class='col-x-12 col-s-12 col-m-3 col-l-3 col-xl-3'>Anzahl Klicks</div>".PHP_EOL.
 "<div class='col-x-12 col-s-12 col-m-9 col-l-9 col-xl-9'>".$row['cat_count']."</div>".PHP_EOL.
 "<div class='col-x-12 col-s-12 col-m-0 col-l-0 col-xl-0'><div class='spacer-s'></div></div>".PHP_EOL.
 "</div>".PHP_EOL;
 
-$content.= "<div class='row hover'>".PHP_EOL.
+$content.= "<div class='row hover bordered'>".PHP_EOL.
 "<div class='col-x-12 col-s-12 col-m-3 col-l-3 col-xl-3'>Klicks heute</div>".PHP_EOL.
 "<div class='col-x-12 col-s-12 col-m-9 col-l-9 col-xl-9'>".$row['clicks_today']."</div>".PHP_EOL.
 "<div class='col-x-12 col-s-12 col-m-0 col-l-0 col-xl-0'><div class='spacer-s'></div></div>".PHP_EOL.
@@ -49,9 +55,14 @@ $content.= "<div class='spacer-m'></div>".PHP_EOL;
 $content.= "<h1>Am meisten geklickte Rezepte</h1>".PHP_EOL;
 $result = mysqli_query($dbl, "SELECT * FROM `most_clicked`") OR DIE(MYSQLI_ERROR($dbl));
 $top = 0;
+$content.= "<div class='row highlight bold bordered'>".PHP_EOL.
+"<div class='col-x-12 col-s-12 col-m-3 col-l-3 col-xl-3'>Platz / Klicks</div>".PHP_EOL.
+"<div class='col-x-12 col-s-12 col-m-9 col-l-9 col-xl-9'>Rezept</div>".PHP_EOL.
+"<div class='col-x-12 col-s-12 col-m-0 col-l-0 col-xl-0'><div class='spacer-s'></div></div>".PHP_EOL.
+"</div>".PHP_EOL;
 while($row = mysqli_fetch_array($result)) {
   $top++;
-  $content.= "<div class='row hover'>".PHP_EOL.
+  $content.= "<div class='row hover bordered'>".PHP_EOL.
   "<div class='col-x-12 col-s-12 col-m-3 col-l-3 col-xl-3'><span class='highlight'>#".$top."</span> (".$row['c']." Klicks)</div>".PHP_EOL.
   "<div class='col-x-12 col-s-12 col-m-9 col-l-9 col-xl-9'><a href='/rezept/".$row['shortTitle']."' target='_blank'>".$row['title']."</a></div>".PHP_EOL.
   "<div class='col-x-12 col-s-12 col-m-0 col-l-0 col-xl-0'><div class='spacer-s'></div></div>".PHP_EOL.
@@ -63,9 +74,15 @@ $content.= "<div class='spacer-m'></div>".PHP_EOL;
 $content.= "<h1>Am besten bewertete Rezepte</h1>".PHP_EOL;
 $result = mysqli_query($dbl, "SELECT * FROM `best_voted`") OR DIE(MYSQLI_ERROR($dbl));
 $top = 0;
+$content.= "<div class='row highlight bold bordered'>".PHP_EOL.
+"<div class='col-x-12 col-s-12 col-m-2 col-l-2 col-xl-2'>Platz / Sterne</div>".PHP_EOL.
+"<div class='col-x-12 col-s-12 col-m-3 col-l-2 col-xl-2'>Sterne</div>".PHP_EOL.
+"<div class='col-x-12 col-s-12 col-m-7 col-l-8 col-xl-8'>Rezept</div>".PHP_EOL.
+"<div class='col-x-12 col-s-12 col-m-0 col-l-0 col-xl-0'><div class='spacer-s'></div></div>".PHP_EOL.
+"</div>".PHP_EOL;
 while($row = mysqli_fetch_array($result)) {
   $top++;
-  $content.= "<div class='row hover'>".PHP_EOL.
+  $content.= "<div class='row hover bordered'>".PHP_EOL.
   "<div class='col-x-12 col-s-12 col-m-2 col-l-2 col-xl-2'><span class='highlight'>#".$top."</span> (".$row['a']." Sterne)</div>".PHP_EOL.
   "<div class='col-x-12 col-s-12 col-m-3 col-l-2 col-xl-2'>".stars($row['a'])."</div>".PHP_EOL.
   "<div class='col-x-12 col-s-12 col-m-7 col-l-8 col-xl-8'><a href='/rezept/".$row['shortTitle']."' target='_blank'>".$row['title']."</a></div>".PHP_EOL.
@@ -77,8 +94,13 @@ $result = mysqli_query($dbl, "SELECT `id`, `title` FROM `categories` WHERE NOT E
 if(mysqli_num_rows($result) != 0) {
   $content.= "<div class='spacer-m'></div>".PHP_EOL;
   $content.= "<h1 class='warn'>Leere Kategorien</h1>".PHP_EOL;
+  $content.= "<div class='row highlight bold bordered'>".PHP_EOL.
+  "<div class='col-x-12 col-s-12 col-m-9 col-l-9 col-xl-9'>Titel</div>".PHP_EOL.
+  "<div class='col-x-12 col-s-12 col-m-3 col-l-3 col-xl-3'>Aktionen</div>".PHP_EOL.
+  "<div class='col-x-12 col-s-12 col-m-0 col-l-0 col-xl-0'><div class='spacer-s'></div></div>".PHP_EOL.
+  "</div>".PHP_EOL;
   while($row = mysqli_fetch_array($result)) {
-    $content.= "<div class='row hover'>".PHP_EOL.
+    $content.= "<div class='row hover bordered'>".PHP_EOL.
     "<div class='col-x-12 col-s-12 col-m-9 col-l-9 col-xl-9'>".$row['title']."</div>".PHP_EOL.
     "<div class='col-x-12 col-s-12 col-m-3 col-l-3 col-xl-3'><a href='/admincategories/edit/".$row['id']."' class='nowrap'>Bearbeiten</a><br><a href='/admincategories/assign/".$row['id']."' class='nowrap'>Rezepte zuweisen</a></div>".PHP_EOL.
     "<div class='col-x-12 col-s-12 col-m-0 col-l-0 col-xl-0'><div class='spacer-s'></div></div>".PHP_EOL.
@@ -90,13 +112,17 @@ $result = mysqli_query($dbl, "SELECT `id`, `title` FROM `items` WHERE NOT EXISTS
 if(mysqli_num_rows($result) != 0) {
   $content.= "<div class='spacer-m'></div>".PHP_EOL;
   $content.= "<h1 class='warn'>Nicht in Kategorien eingeteilte Rezepte</h1>".PHP_EOL;
+  $content.= "<div class='row highlight bold bordered'>".PHP_EOL.
+  "<div class='col-x-12 col-s-12 col-m-9 col-l-9 col-xl-9'>Titel</div>".PHP_EOL.
+  "<div class='col-x-12 col-s-12 col-m-3 col-l-3 col-xl-3'>Aktionen</div>".PHP_EOL.
+  "<div class='col-x-12 col-s-12 col-m-0 col-l-0 col-xl-0'><div class='spacer-s'></div></div>".PHP_EOL.
+  "</div>".PHP_EOL;
   while($row = mysqli_fetch_array($result)) {
-    $content.= "<div class='row hover'>".PHP_EOL.
+    $content.= "<div class='row hover bordered'>".PHP_EOL.
     "<div class='col-x-12 col-s-12 col-m-9 col-l-9 col-xl-9'>".$row['title']."</div>".PHP_EOL.
     "<div class='col-x-12 col-s-12 col-m-3 col-l-3 col-xl-3'><a href='/adminitems/edit/".$row['id']."' class='nowrap'>Bearbeiten</a><br><a href='/adminitems/assign/".$row['id']."' class='nowrap'>Zuweisen</a></div>".PHP_EOL.
     "<div class='col-x-12 col-s-12 col-m-0 col-l-0 col-xl-0'><div class='spacer-s'></div></div>".PHP_EOL.
     "</div>".PHP_EOL;
   }
 }
-
 ?>
