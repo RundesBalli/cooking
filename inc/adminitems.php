5466af5d5a280773d3c0413d78027178a0b2caf0
RundesBalli <rundesballi@rundesballi.com>
2019-10-12 18:00:11 +0200

    Titel hinzugefügt.

--- inc/adminitems.php
+++ inc/adminitems.php
@@ -20,22 +20,40 @@ if(!isset($_GET['action'])) {
   /**
    * Auflisten aller Rezepte.
    */
+  $title = "Rezepte anzeigen";
+  $content.= "<h1>Rezepte anzeigen</h1>".PHP_EOL;
+  $content.= "<div class='row'>".PHP_EOL.
+  "<div class='col-x-12 col-s-12 col-m-12 col-l-12 col-xl-12'><a href='/adminitems/add'>Anlegen</a></div>".PHP_EOL.
+  "</div>".PHP_EOL;
+  $content.= "<div class='spacer-m'></div>".PHP_EOL;
 } elseif($_GET['action'] == 'add') {
   /**
    * Hinzufügen eines Rezepts.
    */
+  $title = "Rezept hinzufügen";
+  $content.= "<h1>Rezept hinzufügen</h1>".PHP_EOL;
+  
 } elseif($_GET['action'] == 'del') {
   /**
    * Löschen eines Rezepts.
    */
+  $title = "Rezept löschen";
+  $content.= "<h1>Rezept löschen</h1>".PHP_EOL;
+  
 } elseif($_GET['action'] == 'edit') {
   /**
    * Bearbeiten eines Rezepts.
    */
+  $title = "Rezept bearbeiten";
+  $content.= "<h1>Rezept bearbeiten</h1>".PHP_EOL;
+  
 } elseif($_GET['action'] == 'assign') {
   /**
    * Zuweisen eines Rezepts in eine Kategorie.
    */
+  $title = "Rezept einer Kategorie hinzufügen";
+  $content.= "<h1>Rezept einer Kategorie hinzufügen</h1>".PHP_EOL;
+  
 } else {
   /**
    * Umleitung falls eine action übergeben wurde, aber nichts zutrifft.
