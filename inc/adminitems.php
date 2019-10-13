ae143c65a2e875c740a62da9953982f1db0b99c4
RundesBalli <rundesballi@rundesballi.com>
2019-10-13 21:26:59 +0200

    Editierfunktion hinzugefügt.

--- inc/adminitems.php
+++ inc/adminitems.php
@@ -369,7 +369,244 @@ if(!isset($_GET['action'])) {
    */
   $title = "Rezept bearbeiten";
   $content.= "<h1>Rezept bearbeiten</h1>".PHP_EOL;
-  
+  $id = (int)defuse($_GET['id']);
+  /**
+   * Prüfen ob das Rezept existiert.
+   */
+  $rezeptresult = mysqli_query($dbl, "SELECT * FROM `items` WHERE `id`='".$id."' LIMIT 1") OR DIE(MYSQLI_ERROR($dbl));
+  if(mysqli_num_rows($rezeptresult) == 0) {
+    /**
+     * Falls das Rezept nicht existiert, wird ein 404er und eine Fehlermeldung zurückgegeben.
+     */
+    http_response_code(404);
+    $content.= "<div class='warnbox'>Das Rezept mit der ID <span class='italic'>".$id."</span> existiert nicht.</div>".PHP_EOL;
+    $content.= "<div class='row'>".PHP_EOL.
+    "<div class='col-x-12 col-s-12 col-m-12 col-l-12 col-xl-12'><a href='/adminitems/list'>Zurück zur Übersicht</a></div>".PHP_EOL.
+    "</div>".PHP_EOL;
+  } else {
+    /**
+     * Wenn das Rezept existiert, dann prüfe ob das Formular übergeben wurde.
+     * Wir gehen davon aus, dass alles okay ist, demzufolge muss das Formular nicht mehr angezeigt werden.
+     * Im Fehlerfall wird das Formular nochmals angezeigt.
+     */
+    $form = 0;
+    if(isset($_POST['submit'])) {
+      /**
+       * Auswertung. Falls alles ok dann $form auf 0 lassen, sonst 1. Bei 0 am Ende wird der Query ausgeführt.
+       */
+      /**
+       * Titel
+       */
+      if(preg_match('/^.{5,100}$/', $_POST['title'], $match) === 1) {
+        $form_title = defuse($match[0]);
+      } else {
+        $form = 1;
+        $content.= "<div class='warnbox'>Der Name des Rezepts ist ungültig. Er muss zwischen 5 und 100 Zeichen lang sein.</div>".PHP_EOL;
+      }
+      /**
+       * Kurztitel
+       */
+      if(preg_match('/^[0-9a-z-_]{5,64}$/', $_POST['shortTitle'], $match) === 1) {
+        $shortTitle = defuse($match[0]);
+      } else {
+        $form = 1;
+        $content.= "<div class='warnbox'>Der Kurztitel ist ungültig. Er muss zwischen 5 und 64 Zeichen lang sein und darf nur aus <code>0-9a-z-_</code> bestehen.</div>".PHP_EOL;
+      }
+      /**
+       * Text
+       */
+      if(!empty(trim($_POST['text']))) {
+        $text = defuse($_POST['text']);
+      } else {
+        $content.= "<div class='infobox'>Der Text ist leer. Rezept wird ohne Inhalt angelegt.</div>".PHP_EOL;
+        $text = NULL;
+      }
+      /**
+       * Personenanzahl
+       */
+      if(!empty($_POST['persons'])) {
+        $persons = (int)defuse($_POST['persons']);
+        if($persons < 1) {
+          $form = 1;
+          $content.= "<div class='warnbox'>Die Angabe der Personenanzahl ist ungültig.</div>".PHP_EOL;
+        } elseif($persons > 10) {
+          $content.= "<div class='infobox'>Für mehr als 10 Personen? Bist du dir sicher?</div>".PHP_EOL;
+        }
+      } else {
+        $form = 1;
+        $content.= "<div class='warnbox'>Die Angabe der Personenanzahl ist ungültig.</div>".PHP_EOL;
+      }
+      /**
+       * Kosten
+       */
+      if(!empty($_POST['cost'])) {
+        $cost = (int)defuse($_POST['cost']);
+        $result = mysqli_query($dbl, "SELECT `id` FROM `meta_cost` WHERE `id`='".$cost."' LIMIT 1") OR DIE(MYSQLI_ERROR($dbl));
+        if(mysqli_num_rows($result) == 0) {
+          $form = 1;
+          $content.= "<div class='warnbox'>Die Angabe der Kosten ist ungültig.</div>".PHP_EOL;
+        }
+      } else {
+        $form = 1;
+        $content.= "<div class='warnbox'>Die Angabe der Kosten ist ungültig.</div>".PHP_EOL;
+      }
+      /**
+       * Schwierigkeit
+       */
+      if(!empty($_POST['difficulty'])) {
+        $difficulty = (int)defuse($_POST['difficulty']);
+        $result = mysqli_query($dbl, "SELECT `id` FROM `meta_difficulty` WHERE `id`='".$difficulty."' LIMIT 1") OR DIE(MYSQLI_ERROR($dbl));
+        if(mysqli_num_rows($result) == 0) {
+          $form = 1;
+          $content.= "<div class='warnbox'>Die Angabe der Schwierigkeit ist ungültig.</div>".PHP_EOL;
+        }
+      } else {
+        $form = 1;
+        $content.= "<div class='warnbox'>Die Angabe der Schwierigkeit ist ungültig.</div>".PHP_EOL;
+      }
+      /**
+       * Dauer
+       */
+      if(!empty($_POST['duration'])) {
+        $duration = (int)defuse($_POST['duration']);
+        $result = mysqli_query($dbl, "SELECT `id` FROM `meta_duration` WHERE `id`='".$duration."' LIMIT 1") OR DIE(MYSQLI_ERROR($dbl));
+        if(mysqli_num_rows($result) == 0) {
+          $form = 1;
+          $content.= "<div class='warnbox'>Die Angabe der Dauer ist ungültig.</div>".PHP_EOL;
+        }
+      } else {
+        $form = 1;
+        $content.= "<div class='warnbox'>Die Angabe der Dauer ist ungültig.</div>".PHP_EOL;
+      }
+      if($form == 0) {
+        /**
+         * Wenn durch die Postdaten-Validierung die Inhalte geprüft und entschärft wurden, kann der Query erzeugt und ausgeführt werden.
+         */
+        if(mysqli_query($dbl, "UPDATE `items` SET `title`='".$form_title."', `shortTitle`='".$shortTitle."', `text`=".($text === NULL ? "NULL" : "'".$text."'").", `persons`='".$persons."', `cost`='".$cost."', `difficulty`='".$difficulty."', `duration`='".$duration."' WHERE `id`='".$id."' LIMIT 1")) {
+          $content.= "<div class='successbox'>Rezept erfolgreich geändert.</div>".PHP_EOL;
+          $content.= "<div class='row'>".PHP_EOL.
+          "<div class='col-x-12 col-s-12 col-m-12 col-l-12 col-xl-12'><a href='/adminitems/list'>Zurück zur Übersicht</a></div>".PHP_EOL.
+          "</div>".PHP_EOL;
+        } else {
+          $form = 1;
+          if(mysqli_errno($dbl) == 1062) {
+            $content.= "<div class='warnbox'>Es existiert bereits ein Rezept mit diesem Kurztitel.</div>".PHP_EOL;
+          } else {
+            $content.= "<div class='warnbox'>Unbekannter Fehler: ".mysqli_error($dbl)."</div>".PHP_EOL;
+          }
+        }
+      }
+    } else {
+      /**
+       * Erstaufruf = Formular wird angezeigt und befüllt.
+       */
+      $form = 1;
+      $row = mysqli_fetch_array($rezeptresult);
+    }
+    /**
+     * Das Formular wird beim Erstaufruf und bei Fehleingaben angezeigt.
+     */
+    if($form == 1) {
+      $content.= "<form action='/adminitems/edit/".$id."' method='post' autocomplete='off'>".PHP_EOL;
+      /**
+       * Tabellenüberschrift
+       */
+      $content.= "<div class='row highlight bold bordered'>".PHP_EOL.
+      "<div class='col-x-12 col-s-12 col-m-4 col-l-3 col-xl-2'>Bezeichnung</div>".PHP_EOL.
+      "<div class='col-x-12 col-s-12 col-m-4 col-l-4 col-xl-4'>Feld</div>".PHP_EOL.
+      "<div class='col-x-12 col-s-12 col-m-4 col-l-5 col-xl-6'>Ergänzungen</div>".PHP_EOL.
+      "<div class='col-x-12 col-s-12 col-m-0 col-l-0 col-xl-0'><div class='spacer-s'></div></div>".PHP_EOL.
+      "</div>".PHP_EOL;
+      /**
+       * Titel
+       */
+      $content.= "<div class='row hover bordered'>".PHP_EOL.
+      "<div class='col-x-12 col-s-12 col-m-4 col-l-3 col-xl-2'>Name des Rezepts</div>".PHP_EOL.
+      "<div class='col-x-12 col-s-12 col-m-4 col-l-4 col-xl-4'><input type='text' name='title' placeholder='Name des Rezepts' tabindex='1' autofocus value='".(isset($row['title']) ? output($row['title']) : (isset($_POST['title']) && !empty($_POST['title']) ? output($_POST['title']) : NULL))."'></div>".PHP_EOL.
+      "<div class='col-x-12 col-s-12 col-m-4 col-l-5 col-xl-6'>Angezeigter Name in der Kategorie<br>5 bis 100 Zeichen</div>".PHP_EOL.
+      "<div class='col-x-12 col-s-12 col-m-0 col-l-0 col-xl-0'><div class='spacer-s'></div></div>".PHP_EOL.
+      "</div>".PHP_EOL;
+      /**
+       * Kurztitel
+       */
+      $content.= "<div class='row hover bordered'>".PHP_EOL.
+      "<div class='col-x-12 col-s-12 col-m-4 col-l-3 col-xl-2'>Kurztitel für URL</div>".PHP_EOL.
+      "<div class='col-x-12 col-s-12 col-m-4 col-l-4 col-xl-4'><input type='text' name='shortTitle' placeholder='/rezept/xxx' tabindex='2' value='".(isset($row['shortTitle']) ? output($row['shortTitle']) : (isset($_POST['shortTitle']) && !empty($_POST['shortTitle']) ? output($_POST['shortTitle']) : NULL))."'></div>".PHP_EOL.
+      "<div class='col-x-12 col-s-12 col-m-4 col-l-5 col-xl-6'>".Slimdown::render("`/rezept/&lt;Kurztitel&gt;`\n* muss einzigartig sein\n* 5 bis 64 Zeichen\n* keine Leerzeichen\n* keine Umlaute\n* keine Sonderzeichen\n* nur Kleinbuchstaben oder Zahlen\n* zur Worttrennung `-` oder `_` benutzen\n`0-9a-z-_`")."</div>".PHP_EOL.
+      "<div class='col-x-12 col-s-12 col-m-0 col-l-0 col-xl-0'><div class='spacer-s'></div></div>".PHP_EOL.
+      "</div>".PHP_EOL;
+      /**
+       * Text
+       */
+      $content.= "<div class='row hover bordered'>".PHP_EOL.
+      "<div class='col-x-12 col-s-12 col-m-4 col-l-3 col-xl-2'>Text</div>".PHP_EOL.
+      "<div class='col-x-12 col-s-12 col-m-4 col-l-4 col-xl-4'><textarea name='text' placeholder='Mehrzeiliger Text' tabindex='3'>".(isset($row['text']) ? output($row['text']) : (isset($_POST['text']) && !empty($_POST['text']) ? output($_POST['text']) : NULL))."</textarea></div>".PHP_EOL.
+      "<div class='col-x-12 col-s-12 col-m-4 col-l-5 col-xl-6'>".Slimdown::render("* [Markdown für mehrzeilige Textfelder](/adminmarkdowninfo)* möglich\n* Das hier ist das eigentliche Rezept. Der Haupttext.")."</div>".PHP_EOL.
+      "<div class='col-x-12 col-s-12 col-m-0 col-l-0 col-xl-0'><div class='spacer-s'></div></div>".PHP_EOL.
+      "</div>".PHP_EOL;
+      /**
+       * Personenanzahl
+       */
+      $content.= "<div class='row hover bordered'>".PHP_EOL.
+      "<div class='col-x-12 col-s-12 col-m-4 col-l-3 col-xl-2'>Personenanzahl</div>".PHP_EOL.
+      "<div class='col-x-12 col-s-12 col-m-4 col-l-4 col-xl-4'><input type='number' name='persons' placeholder='z.B. 4' tabindex='4' min='1' value='".(isset($row['persons']) ? output($row['persons']) : (isset($_POST['persons']) && !empty($_POST['persons']) ? output($_POST['persons']) : NULL))."'></div>".PHP_EOL.
+      "<div class='col-x-12 col-s-12 col-m-4 col-l-5 col-xl-6'>".Slimdown::render("* Möglich sind alle positiven Zahlen\n* bei über 10 Personen wird eine Info angezeigt, das Rezept wird aber angelegt.")."</div>".PHP_EOL.
+      "<div class='col-x-12 col-s-12 col-m-0 col-l-0 col-xl-0'><div class='spacer-s'></div></div>".PHP_EOL.
+      "</div>".PHP_EOL;
+      /**
+       * Kosten
+       */
+      $content.= "<div class='row hover bordered'>".PHP_EOL.
+      "<div class='col-x-12 col-s-12 col-m-4 col-l-3 col-xl-2'>Kosten</div>".PHP_EOL.
+      "<div class='col-x-12 col-s-12 col-m-4 col-l-4 col-xl-4'><select name='cost' tabindex='5'>".PHP_EOL."<option value='' selected disabled hidden>Bitte wählen</option>".PHP_EOL;
+      $innerresult = mysqli_query($dbl, "SELECT * FROM `meta_cost` ORDER BY `id` ASC") OR DIE(MYSQLI_ERROR($dbl));
+      while($innerrow = mysqli_fetch_array($innerresult)) {
+        $content.= "<option value='".$innerrow['id']."'".(isset($row['cost']) ? ($row['cost'] == $innerrow['id'] ? " selected" : NULL) : ((isset($_POST['cost']) && !empty($_POST['cost']) AND $innerrow['id'] == $_POST['cost']) ? " selected" : NULL)).">".output($innerrow['title'])."</option>".PHP_EOL;
+      }
+      $content.= "</select></div>".PHP_EOL.
+      "<div class='col-x-12 col-s-12 col-m-4 col-l-5 col-xl-6'></div>".PHP_EOL.
+      "<div class='col-x-12 col-s-12 col-m-0 col-l-0 col-xl-0'><div class='spacer-s'></div></div>".PHP_EOL.
+      "</div>".PHP_EOL;
+      /**
+       * Schwierigkeit
+       */
+      $content.= "<div class='row hover bordered'>".PHP_EOL.
+      "<div class='col-x-12 col-s-12 col-m-4 col-l-3 col-xl-2'>Schwierigkeit</div>".PHP_EOL.
+      "<div class='col-x-12 col-s-12 col-m-4 col-l-4 col-xl-4'><select name='difficulty' tabindex='6'>".PHP_EOL."<option value='' selected disabled hidden>Bitte wählen</option>".PHP_EOL;
+      $innerresult = mysqli_query($dbl, "SELECT * FROM `meta_difficulty` ORDER BY `id` ASC") OR DIE(MYSQLI_ERROR($dbl));
+      while($innerrow = mysqli_fetch_array($innerresult)) {
+        $content.= "<option value='".$innerrow['id']."'".(isset($row['difficulty']) ? ($row['difficulty'] == $innerrow['id'] ? " selected" : NULL) : ((isset($_POST['difficulty']) && !empty($_POST['difficulty']) AND $innerrow['id'] == $_POST['difficulty']) ? " selected" : NULL)).">".output($innerrow['title'])."</option>".PHP_EOL;
+      }
+      $content.= "</select></div>".PHP_EOL.
+      "<div class='col-x-12 col-s-12 col-m-4 col-l-5 col-xl-6'></div>".PHP_EOL.
+      "<div class='col-x-12 col-s-12 col-m-0 col-l-0 col-xl-0'><div class='spacer-s'></div></div>".PHP_EOL.
+      "</div>".PHP_EOL;
+      /**
+       * Dauer
+       */
+      $content.= "<div class='row hover bordered'>".PHP_EOL.
+      "<div class='col-x-12 col-s-12 col-m-4 col-l-3 col-xl-2'>Dauer</div>".PHP_EOL.
+      "<div class='col-x-12 col-s-12 col-m-4 col-l-4 col-xl-4'><select name='duration' tabindex='7'>".PHP_EOL."<option value='' selected disabled hidden>Bitte wählen</option>".PHP_EOL;
+      $innerresult = mysqli_query($dbl, "SELECT * FROM `meta_duration` ORDER BY `id` ASC") OR DIE(MYSQLI_ERROR($dbl));
+      while($innerrow = mysqli_fetch_array($innerresult)) {
+        $content.= "<option value='".$innerrow['id']."'".(isset($row['duration']) ? ($row['duration'] == $innerrow['id'] ? " selected" : NULL) : ((isset($_POST['duration']) && !empty($_POST['duration']) AND $innerrow['id'] == $_POST['duration']) ? " selected" : NULL)).">".output($innerrow['title'])."</option>".PHP_EOL;
+      }
+      $content.= "</select></div>".PHP_EOL.
+      "<div class='col-x-12 col-s-12 col-m-4 col-l-5 col-xl-6'></div>".PHP_EOL.
+      "<div class='col-x-12 col-s-12 col-m-0 col-l-0 col-xl-0'><div class='spacer-s'></div></div>".PHP_EOL.
+      "</div>".PHP_EOL;
+      /**
+       * Absenden
+       */
+      $content.= "<div class='row hover bordered'>".PHP_EOL.
+      "<div class='col-x-12 col-s-12 col-m-4 col-l-3 col-xl-2'>Rezept ändern</div>".PHP_EOL.
+      "<div class='col-x-12 col-s-12 col-m-4 col-l-4 col-xl-4'><input type='submit' name='submit' value='Ändern' tabindex='8'></div>".PHP_EOL.
+      "<div class='col-x-12 col-s-12 col-m-4 col-l-5 col-xl-6'></div>".PHP_EOL.
+      "<div class='col-x-12 col-s-12 col-m-0 col-l-0 col-xl-0'><div class='spacer-s'></div></div>".PHP_EOL.
+      "</div>".PHP_EOL;
+      $content.= "</form>".PHP_EOL;
+    }
+  }
 } elseif($_GET['action'] == 'assign') {
   /**
    * Zuweisen eines Rezepts in eine Kategorie.
