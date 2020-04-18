71f345d341456c6d45be387a082fe678d57a0606
RundesBalli <rundesballi@rundesballi.com>
2020-04-18 23:06:48 +0200

    Links verändert

--- public/inc/start.php
+++ public/inc/start.php
@@ -19,7 +19,7 @@ $content.= "<div class='spacer-m'></div>".PHP_EOL;
  */
 $content.= "<h1><span class='fas icon'>&#xf0c0;</span>Team</h1>".PHP_EOL;
 $content.= "<div class='row hover'>".PHP_EOL.
-"<div class='col-x-6 col-s-6 col-m-5 col-l-4 col-xl-3'><a href='https://pr0gramm.com/inbox/messages/Nezos' target='_blank' rel='noopener'>Nezos</a></div>".PHP_EOL.
+"<div class='col-x-6 col-s-6 col-m-5 col-l-4 col-xl-3'><a href='https://Nezos.wtf' target='_blank' rel='noopener'>Nezos</a></div>".PHP_EOL.
 "<div class='col-x-6 col-s-6 col-m-7 col-l-8 col-xl-9'>Rezepte, Bilder, Erstausarbeitung</div>".PHP_EOL.
 "</div>".PHP_EOL;
 $content.= "<div class='row hover'>".PHP_EOL.
@@ -27,7 +27,7 @@ $content.= "<div class='row hover'>".PHP_EOL.
 "<div class='col-x-6 col-s-6 col-m-7 col-l-8 col-xl-9'>Sichtung, Korrektur, Text</div>".PHP_EOL.
 "</div>".PHP_EOL;
 $content.= "<div class='row hover'>".PHP_EOL.
-"<div class='col-x-6 col-s-6 col-m-5 col-l-4 col-xl-3'><a href='https://pr0gramm.com/inbox/messages/RundesBalli' target='_blank' rel='noopener'>RundesBalli</a></div>".PHP_EOL.
+"<div class='col-x-6 col-s-6 col-m-5 col-l-4 col-xl-3'><a href='https://RundesBalli.com' target='_blank' rel='noopener'>RundesBalli</a></div>".PHP_EOL.
 "<div class='col-x-6 col-s-6 col-m-7 col-l-8 col-xl-9'><a href='https://github.com/RundesBalli/pr0cooking' target='_blank' rel='noopener'>Entwicklung</a> und Hosting</div>".PHP_EOL.
 "</div>".PHP_EOL;
 
