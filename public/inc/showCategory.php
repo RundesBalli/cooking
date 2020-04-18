da1b0bda8a943b8b6af7256992b87d5f92be3541
RundesBalli <rundesballi@rundesballi.com>
2020-04-18 22:58:57 +0200

    Kategorieansicht verändert

--- public/inc/showCategory.php
+++ public/inc/showCategory.php
@@ -52,22 +52,27 @@ if(!isset($_GET['category']) OR empty(trim($_GET['category']))) {
     if(mysqli_num_rows($result) == 0) {
       $content.= "<div class='infobox'>Dieser Kategorie wurden noch keine Rezepte zugewiesen</div>".PHP_EOL;
     } else {
-      $content.= "<div class='row'>".PHP_EOL;
+      $items = array();
       while($row = mysqli_fetch_array($result)) {
-        $content.= "<div class='col-x-12 col-s-12 col-m-6 col-l-4 col-xl-3 item'>".PHP_EOL.
-        "<a href='/rezept/".output($row['shortTitle'])."'>".PHP_EOL.
-        "<img src='/img/".($row['fileHash'] === NULL ? "noThumb.png" : "thumb-".$row['id']."-".$row['fileHash'].".png")."'>".PHP_EOL.
-        "<div><span class='title'>".$row['title']."</span></div>".PHP_EOL.
-        "</a>".PHP_EOL.
-        "<div class='stars'>".stars($row['votes'], $row['voteCount'])."</div>".PHP_EOL.
-        "<div class='specs'><span class='far icon'>&#xf25a;</span>&nbsp;".number_format($row['clicks'], 0, ",", ".")."</div>".PHP_EOL.
-        "<div class='specs'><span class='far icon'>&#xf0eb;</span> ".$row['difficulty']."</div>".PHP_EOL.
-        "<br>".PHP_EOL.
-        "<div class='specs'><span class='far icon'>&#xf254;</span> ".$row['duration']."</div>".PHP_EOL.
-        "<div class='specs'><span class='fas icon'>&#xf153;</span> ".$row['cost']."</div>".PHP_EOL.
+        $items[] =
+        "<div class='row item'>".PHP_EOL.
+          "<div class='col-x-12 col-s-12 col-m-12 col-l-4 col-xl-3'>".PHP_EOL.
+            "<a href='/rezept/".output($row['shortTitle'])."'>".PHP_EOL.
+              "<img src='/img/".($row['fileHash'] === NULL ? "noThumb.png" : "thumb-".$row['id']."-".$row['fileHash'].".png")."'>".PHP_EOL.
+            "</a>".PHP_EOL.
+          "</div>".PHP_EOL.
+          "<div class='col-x-12 col-s-12 col-m-12 col-l-8 col-xl-9 iteminfo'>".PHP_EOL.
+            "<div class='title'><a href='/rezept/".output($row['shortTitle'])."'>".$row['title']."</a></div>".PHP_EOL.
+            "<div class='stars'>".stars($row['votes'], $row['voteCount'])."</div>".PHP_EOL.
+            "<div class='specs'><span class='far icon'>&#xf25a;</span> ".number_format($row['clicks'], 0, ",", ".")."</div>".PHP_EOL.
+            "<div class='specs'><span class='far icon'>&#xf0eb;</span> ".$row['difficulty']."</div>".PHP_EOL.
+            "<br>".PHP_EOL.
+            "<div class='specs'><span class='far icon'>&#xf254;</span> ".$row['duration']."</div>".PHP_EOL.
+            "<div class='specs'><span class='fas icon'>&#xf153;</span> ".$row['cost']."</div>".PHP_EOL.
+          "</div>".PHP_EOL.
         "</div>".PHP_EOL;
       }
-      $content.= "</div>".PHP_EOL;
+      $content.= implode("<hr class='itemhr'>".PHP_EOL, $items);
     }
   } else {
     /**
