89f7f79afa12bbfb1d6633624ec198edcd1f46f6
RundesBalli <GitHub@RundesBalli.com>
2022-03-03 22:56:36 +0100

    Beispielstartseite hinzugefügt

---
+++ public/inc/start.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * start.php
+ * 
+ * Startseite mit kurzer Begrüßung und Erläuterung.
+ */
+$content.= "<h1 class='center'><span class='fas icon'>&#xf805;</span>".$ogConfig['sitename']."</h1>";
+$content.= "<div class='spacer-m'></div>";
+$content.= "<div class='row center'>".
+"<div class='col-s-12 col-l-12'>Herzlich willkommen</div>".
+"</div>";
+?>
